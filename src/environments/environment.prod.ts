429dc7c939ce1c2d5e54def5b8196a895b86553b
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2025-09-02 00:34:39 -0500

    n

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   URL_COUNTRIES: 'https://countriesnow.space/api/v0.1/countries/flag/unicode',
-  URL_NEWS: ' https://newsapi.org/v2/top-headlines?language=en',
+  URL_NEWS: 'https://newsapi.org/v2/top-headlines?language=en',
   SECRET_KEY: 'MY-KEY',
   API_KEY: "644e2184ee494289a74582d00610c30a"
 };
